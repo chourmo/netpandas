a69b38818b921d348667fb7fbfa83c077b2d8749
chourmo <vincenttinet@icloud.com>
2024-03-24 17:43:20 +0100

    update setup to shapely

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 netpandas
 Add graph functionnalities to a Pandas dataframe through its extension capabilities.
 """
+
 import sys
 from setuptools import setup, find_packages
 import versioneer
 
-short_description = "Add graph functionnalities to a Pandas dataframe through its extension capabilities.".split("\n")[0]
+short_description = "Add graph functionnalities to a Pandas dataframe through its extension capabilities.".split(
+    "\n"
+)[
+    0
+]
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 try:
     with open("README.md", "r") as handle:
@@ -21,39 +26,42 @@ except:
 
 setup(
     # Self-descriptive entries which should always be present
-    name='netpandas',
-    author='chourmo',
-    author_email='vincenttinet@mac.com',
+    name="netpandas",
+    author="chourmo",
+    author_email="vincenttinet@mac.com",
     description=short_description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='MIT',
-
+    license="MIT",
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
     packages=find_packages(),
-
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
     include_package_data=True,
-
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-
     # Additional entries you may want simply uncomment the lines you want and fill in the data
-    url='https://github.com/chourmo/netpandas',
-    install_requires=["numpy", "pandas", "scipy", "scikit-learn", "geopandas", "pygeos", "pandana", "networkx"],
+    url="https://github.com/chourmo/netpandas",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "geopandas",
+        "shapely",
+        "pandana",
+        "networkx",
+    ],
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
     #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    python_requires=">=3.9",          # Python version restrictions
-
+    python_requires=">=3.9",  # Python version restrictions
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     # zip_safe=False,
-
 )
